6c19508776696794bad95f36e6694c4a39f598b2
Jonathan Couldridge <jcouldridge@outlook.com>
2018-04-20 09:51:49 +0100

    bootstrap app works as intended, doesn't queue in redis yet

--- boostrap/app/index.js
+++ boostrap/app/index.js
@@ -1,1 +1,17 @@
-console.log('hello')
+// add timestamps in front of log messages
+require("console-stamp")(console, "HH:MM:ss.l");
+const http = require("http");
+
+http
+  .createServer(function(req, res) {
+    if (req.url == "/initiate" && req.method == "POST") {
+      console.log("initiation request received");
+      res.writeHead(202, { "Content-Type": "text/plain" });
+      res.end("accepted");
+    } else {
+      console.log("bad request received")
+      res.writeHead(400, { "Content-Type": "text/plain" });
+      res.end("bad request - please POST to /initiate");
+    }
+  })
+  .listen(5000);
