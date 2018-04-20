8009b66b70d7a74ff7a3c6dea3c823d1fc1c31c3
Jonathan Couldridge <jcouldridge@outlook.com>
2018-04-20 09:30:41 +0100

    incomplete dotnet watcher only see existing files not new

--- watcher-dotnet/Program.cs
+++ watcher-dotnet/Program.cs
@@ -6,7 +6,7 @@ namespace watcher_dotnet
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("change detected");
         }
     }
 }
