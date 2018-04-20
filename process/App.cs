adb7910dd9e2c660d30445aa395976fb94a77f56
Jonathan Couldridge <jcouldridge@outlook.com>
2018-04-20 20:37:36 +0100

    theoretically dotnet app checks mqapi is up now

--- process/App.cs
+++ process/App.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using process.Services;
@@ -20,14 +22,33 @@ namespace process
         {
             _logger.LogInformation("App started");
 
+            // First keep trying to get queues out of the mq api to ensure it's up
+            var mqUp = false;
+            while(!mqUp)
+            {
+                try
+                {
+                    await TryGetQueues();
+                    mqUp = true;
+                } catch (HttpRequestException e)
+                {
+                    _logger.LogError(e.Message);
+                    _logger.LogError(e.InnerException?.Message ?? "no inner exception");
+                    Thread.Sleep(3000); // wait to retry
+                }
+            }
+
+            Console.Read();
+        }
+
+        private async Task TryGetQueues()
+        {
             var queues = await _redis.ListQueues();
 
             foreach (var queue in queues)
             {
                 _logger.LogInformation($"found queue: {queue}");
             }
-
-            Console.Read();
         }
     }
 }
