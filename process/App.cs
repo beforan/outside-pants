34cf0dd779d876b423f38702b06155c4c743b050
Jonathan Couldridge <jcouldridge@outlook.com>
2018-04-20 14:06:32 +0100

    logging, di, config in dotnet console app

---
+++ process/App.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+
+namespace process
+{
+    public class App
+    {
+        private readonly ILogger _logger;
+
+        public App(ILogger<App> logger)
+        {
+            _logger = logger;
+        }
+
+        public void Run()
+        {
+            _logger.LogInformation("App started");
+            Console.Read();
+        }
+    }
+}
--- process/Program.cs
+++ process/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace process
 {
@@ -6,7 +9,33 @@ namespace process
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Create service collection
+            var serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+
+            // Create service provider
+            serviceCollection.BuildServiceProvider()
+                .GetService<App>().Run();
+        }
+
+        private static void ConfigureServices(IServiceCollection serviceCollection)
+        {
+            // Build configuration
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(AppContext.BaseDirectory)
+                .AddJsonFile("appsettings.json", false)
+                .Build();
+
+            // Add services
+            serviceCollection.AddSingleton(configuration);
+            serviceCollection.AddLogging(builder =>
+            {
+                builder.AddConfiguration(configuration.GetSection("Logging"));
+                builder.AddConsole();
+            });
+
+            // Add the App
+            serviceCollection.AddTransient<App>();
         }
     }
 }
